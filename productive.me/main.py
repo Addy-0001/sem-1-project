8c362e619f033cb268fbf05847e628bbe78be21b
Adamya Neupane <adamsneupane@gmail.com>
2023-05-15 12:37:23 +0545

    added database and some more files

--- productive.me/main.py
+++ productive.me/main.py
@@ -1,1 +1,12 @@
 from . import timer
+from .models import Model 
+
+# #make the timer module
+
+def timer():
+    timer1 = 25
+    timer2 = 5
+    timer3 = 15
+    timer4 = 30
+    timer5 = 60
+    
