5a5d0e8cfb13972fa039cb3288576bd2681ad9c0
Adamya Neupane <adamsneupane@gmail.com>
2023-05-16 07:37:18 +0545

    made login windows

--- productive.me/models.py
+++ productive.me/models.py
@@ -1,47 +1,13 @@
 import sqlite3
+#connect the database to the app
+conn = sqlite3.connect('productive.db')
+c = conn.cursor()
 
-connection = sqlite3.connect("company.db")
-print("Opened Database Successfully")
 
-connection.execute('''CREATE TABLE COMPANY
-    (ID INT PRIMARY KEY NOT NULL,
-    NAME TEXT NOT NULL,
-    AGE INT NOT NULL, 
-    ADDRESS CHAR(100),
-    SALARY REAL);''')
-print("Table created successfully")
+# Make a user class that stores the name, email and password of a user
+class User:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
 
-connection.execute("INSERT INTO  COMPANY(ID, NAME, AGE, ADDRESS, SALARY) \
-    VALUES (1, 'ADAMYA', 21, 'NEPAL', 30000.00)")
-
-connection.commit()
-
-print("Records created successfully")
-
-print("Exporting data from the database")
-
-cursor = connection.execute(
-    "SELECT ID, NAME, AGE, ADDRESS, SALARY FROM COMPANY")
-for row in cursor:
-    print("ID = ", row[0])
-    print("NAME = ", row[1])
-    print("AGE = ", row[2])
-    print("ADDRESS = ", row[3])
-    print("SALARY = ", row[4], "\n")
-print("Pring Operation completed Successfully")
-
-# changing the data using UPDATE in the database
-connection.execute("UPDATE COMPANY set SALARY = 50000.00 where ID = 1")
-connection.commit()
-
-cursor = connection.execute(
-    "SELECT ID, NAME, AGE, ADDRESS, SALARY FROM COMPANY")
-for row in cursor:
-    print("ID = ", row[0])
-    print("NAME = ", row[1])
-    print("AGE = ", row[2])
-    print("ADDRESS = ", row[3])
-    print("SALARY = ", row[4], "\n")
-print("Print Operation completed Successfully")
-
-connection.close()
