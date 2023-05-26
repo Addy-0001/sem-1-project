3a77f3ec5c588727b5d739115800eee322566b79
Adamya Neupane <adamsneupane@gmail.com>
2023-05-26 09:19:21 +0545

    base

--- virtuedu/courses/models.py
+++ virtuedu/courses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    number_of_lectures = models.IntegerField()
+    number_of_exercises = models.IntegerField()
+    created_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
