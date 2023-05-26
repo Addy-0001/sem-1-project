e2e012a6f757cd2085b67cbd1da4507ef500c2e1
Adamya Neupane <adamsneupane@gmail.com>
2023-05-26 08:21:00 +0545

    deleted old

--- productive.me/main.py
+++
@@ -1,9 +0,0 @@
-#this is the starting of the application 
-#this is the main file of the application
-from login import * 
-def main():
-    root = Tk()
-    Login(root)
-    root.mainloop()
-if __name__ == "__main__":
-    main()
--- webapp/users/admin.py
+++
@@ -1,4 +0,0 @@
-from .models import studentUser
-from django.contrib import admin
-
-admin.site.register(studentUser)
