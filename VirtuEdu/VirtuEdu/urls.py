943d823bc0adeab086af14eefa1570f798cef193
Adamya Neupane <adamsneupane@gmail.com>
2023-06-02 10:50:54 +0545

    changed data

--- VirtuEdu/VirtuEdu/urls.py
+++ VirtuEdu/VirtuEdu/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     path('admin/', admin.site.urls), #this is the path to the admin site
     path('', views.home, name='home'),
     path("user/", include('users.urls')), #this is the path to the users app level urls.py file
-    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'), name='logout'),
 ]
