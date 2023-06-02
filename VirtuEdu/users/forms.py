0ee207459048d980a5dfb1ce05c0170546bdf819
Adamya Neupane <adamsneupane@gmail.com>
2023-06-02 18:10:42 +0545

    database

--- VirtuEdu/users/forms.py
+++ VirtuEdu/users/forms.py
@@ -2,8 +2,15 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+
 class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=50, required=True, help_text='Required')
+    last_name = forms.CharField(
+        max_length=50, required=True, help_text='Required')
     email = forms.EmailField()
+
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['first_name', 'last_name',
+                  'email', 'password1', 'password2']
